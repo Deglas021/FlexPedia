91cb224c20f0adb8c56f0aa07475a2816103e68e
Deglas021 <Douglasmigsil@gmail.com>
2026-04-09 17:41:06 -0300

    concluindo a issue 1#

---
+++ api/flexpedia/src/test/java/com/flexpedia/flexpedia/FlexpediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexpedia.flexpedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexpediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
